ba01cc24d31e57af292d95e5bbc8fb009a16608f
tanviagr <tanvia29@gmail.com>
2021-05-06 21:47:55 +0530

    refactoring the MappingJacksonValue method block

--- spring-microservices/src/main/java/com/example/demo/filtering/FilteringController.java
+++ spring-microservices/src/main/java/com/example/demo/filtering/FilteringController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,29 +14,42 @@ import java.util.List;
 @RestController
 public class FilteringController
 {
+
+    private static MappingJacksonValue getMappingWithFilters(List<FilteredBean> beanList, String... propertyArray)
+    {
+        MappingJacksonValue mapping = new MappingJacksonValue(beanList);
+        SimpleBeanPropertyFilter filter;
+        if (propertyArray.length == 0)
+        {
+            filter = SimpleBeanPropertyFilter.serializeAll();
+        }
+        else
+        {
+            filter = SimpleBeanPropertyFilter.filterOutAllExcept(propertyArray);
+        }
+        FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+
+    private static MappingJacksonValue getMappingWithFilters(FilteredBean bean, String... propertyArray)
+    {
+        List<FilteredBean> listBeans = Arrays.asList(bean);
+        return getMappingWithFilters(listBeans, propertyArray);
+    }
 //    only want to return field1, field2
     @GetMapping(path = "/filteringField3")
     public MappingJacksonValue getField1AndField2()
     {
         FilteredBean bean = new FilteredBean("value1", "value2", "value3");
-        MappingJacksonValue mapping = new MappingJacksonValue(bean);
-
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
-        FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
-        mapping.setFilters(filters);
-
-        return mapping;
+        return getMappingWithFilters(bean, "field1", "field2");
     }
 
     @GetMapping(path = "/filteringField1And2")
     public MappingJacksonValue getField3()
     {
         FilteredBean bean = new FilteredBean("value1", "value2", "value3");
-        MappingJacksonValue mapping = new MappingJacksonValue(bean);
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field3");
-        FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
-        mapping.setFilters(filters);
-        return mapping;
+        return getMappingWithFilters(bean, "field3");
     }
 
     @GetMapping(path = "/filtering-list")
@@ -46,11 +60,7 @@ public class FilteringController
                 new FilteredBean("value11", "value12", "value13"),
                 new FilteredBean("value111", "value222", "value333")
         );
-        MappingJacksonValue mapping = new MappingJacksonValue(beanList);
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2");
-        FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
-        mapping.setFilters(filters);
-        return mapping;
+        return getMappingWithFilters(beanList, "field2");
     }
 
     @GetMapping(path = "/filter-full-view")
@@ -62,10 +72,6 @@ public class FilteringController
                 new FilteredBean("value111", "value222", "value333")
         );
 //        since we have defined a bean level filter - we have to specify it.
-        MappingJacksonValue mapping = new MappingJacksonValue(fullObjects);
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.serializeAll();
-        FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
-        mapping.setFilters(filters);
-        return mapping;
+        return getMappingWithFilters(fullObjects);
     }
 }
