2d1e18ab839ea138e23a892a5e6f42f0015d2c03
tanviagr <tanvia29@gmail.com>
2021-05-06 20:51:33 +0530

    static filtering through @JsonIgnoreProperties

---
+++ spring-microservices/src/main/java/com/example/demo/filtering/FilteredBean.java
@@ -0,0 +1,54 @@
+package com.example.demo.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import net.minidev.json.annotate.JsonIgnore;
+
+@JsonIgnoreProperties(value = {"field2"})
+public class FilteredBean
+{
+    private String field1;
+
+//    this is some sensitive field - dont want to display in GET results
+    private String field2;
+
+    private String field3;
+
+    public FilteredBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+    @Override
+    public String toString() {
+        return "FilteredBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
