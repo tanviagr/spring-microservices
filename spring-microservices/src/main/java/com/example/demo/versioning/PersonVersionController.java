1efda55bf698f404f6366f4f2141ca808ed2c3aa
tanviagr <tanvia29@gmail.com>
2021-05-07 12:54:12 +0530

    MIME type versioning

--- spring-microservices/src/main/java/com/example/demo/versioning/PersonVersionController.java
+++ spring-microservices/src/main/java/com/example/demo/versioning/PersonVersionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersionController
 {
+//  1.  URI VERSIONING
     @GetMapping(path = "v1/person")
     public PersonV1 getPersonV1()
     {
@@ -18,7 +19,8 @@ public class PersonVersionController
         return new PersonV2(new Name("Tanvi", "Agrawal"));
     }
 
-//    doing versioning through request params = http://localhost:8080/person?version=1
+//  2.   doing versioning through request params = http://localhost:8080/person?version=1
+//    REQUEST PARAM VERSIONING
     @GetMapping(value = "person/params", params = "version=1")
     public PersonV1 getPersonV1ThroughParams()
     {
@@ -32,7 +34,7 @@ public class PersonVersionController
         return new PersonV2(new Name("Tanvi", "Agrawal"));
     }
 
-//    versioning through headers
+//  3.   versioning through headers - HEADER VERSIONING -  send through X-API_VERSION header in the request
     @GetMapping(value = "person/header", headers = "X-API-VERSION=1")
     public PersonV1 getPersonV1ThroughHeaders()
     {
@@ -41,6 +43,19 @@ public class PersonVersionController
 
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
     public PersonV2 getPersonV2ThroughHeaders()
+    {
+        return new PersonV2(new Name("Tanvi", "Agrawal"));
+    }
+
+//  4.  versioning through produces - CONTENT NEGOTIATION, MIME TYPE VERSIONING - send through Accept header
+    @GetMapping(value = "person/produces", produces = "application/vnd.tanvi.app-v1+json")
+    public PersonV1 getPersonV1ThroughContentNegotiation()
+    {
+        return new PersonV1("Tanvi Agrawal");
+    }
+
+    @GetMapping(value = "person/produces", produces = "application/vnd.tanvi.app-v2+json")
+    public PersonV2 getPersonV2ThroughContentNegotiation()
     {
         return new PersonV2(new Name("Tanvi", "Agrawal"));
     }
