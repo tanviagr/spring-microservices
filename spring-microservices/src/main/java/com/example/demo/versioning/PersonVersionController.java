c3be27c642e50e59ad855b7e254a252f17e14f74
tanviagr <tanvia29@gmail.com>
2021-05-07 12:37:11 +0530

    versioning through request params

--- spring-microservices/src/main/java/com/example/demo/versioning/PersonVersionController.java
+++ spring-microservices/src/main/java/com/example/demo/versioning/PersonVersionController.java
@@ -17,4 +17,18 @@ public class PersonVersionController
     {
         return new PersonV2(new Name("Tanvi", "Agrawal"));
     }
+
+//    doing versioning through request params = http://localhost:8080/person?version=1
+    @GetMapping(value = "person", params = "version=1")
+    public PersonV1 getPersonV1ThroughParams()
+    {
+        return new PersonV1("Tanvi Agrawal");
+    }
+
+//    http://localhost:8080/person?version=2
+    @GetMapping(value = "person", params = "version=2")
+    public PersonV2 getPersonV2ThroughParams()
+    {
+        return new PersonV2(new Name("Tanvi", "Agrawal"));
+    }
 }
