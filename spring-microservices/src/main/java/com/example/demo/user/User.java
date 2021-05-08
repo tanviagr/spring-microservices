49a2196992a9b3cf77a4f9dbe89375096ce46895
tanviagr <tanvia29@gmail.com>
2021-05-08 13:54:16 +0530

    setting up

--- spring-microservices/src/main/java/com/example/demo/user/User.java
+++ spring-microservices/src/main/java/com/example/demo/user/User.java
@@ -3,10 +3,15 @@ package com.example.demo.user;
 import com.sun.istack.Nullable;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Schema(description =  "This is a social media user")
+@Entity //maintained by JPA - class is an entity and is mapped to a database table.
 public class User
 {
     @Size(min = 2, message =  "name should have atleast 2 characters")
@@ -17,6 +22,8 @@ public class User
     @Schema(description = "Birthdate should be in the past")
     private Date birthDate;
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     public User(String firstName, Date birthDate, Integer id) {
---
+++ spring-microservices/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values(1, sysdate(), 'Rahul');
+insert into user values(2, sysdate(), 'Ankit');
+insert into user values(3, sysdate(), 'Swapnil');
+--can only use single quotes in SQL files - otherwise Gopi not found
+
+--the link to h2 console is http://localhost:8080/h2-console
